0c873d93b1146394e049b809f2554b6aaa391208
Marchese305 <32369907+Marchese305@users.noreply.github.com>
2017-09-28 18:42:53 -0400

    Update GoogleFavorite.py

--- GoogleFavorite.py
+++ GoogleFavorite.py
@@ -1,1 +1,15 @@
 
+import sys
+
+def addFavorite():
+  favorite_list = [] 
+  num = 1
+
+  while (num != 0) :
+    new_favorite = sys.stdin.readline()
+    favorite_list.append(new favorite)
+    num = sys.stdin.readline()
+  print(favorite_list)
+  
+addfavorite()
+  
